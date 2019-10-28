679fecfe68a273021b6f4471001b9e5eb660f719
mikasd <michael.ingley@gmail.com>
2019-10-28 11:22:26 -0700

    started checkcollision method

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -1,3 +1,5 @@
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
+
 export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
@@ -5,3 +7,20 @@ export const createStage = () =>
     Array.from(Array(STAGE_HEIGHT), () => 
         new Array(STAGE_WIDTH).fill([0, 'clear'])
     )
+
+export const checkCollision = (player, stage, {x: moveX, y: moveY}) => {
+    for(let y = 0; y < player.tetromino.length; y++){
+        for(let x = 0; x < player.tetromino[y].length; x++){
+            //check if cell contains tetromino
+            if(player.tetromino[y][x] !== 0){
+            //make sure tetromino doesn't leave stage max height(y)
+
+
+            //make sure tetromino doesn't leave stage max width(x)
+
+            
+
+            }
+        }
+    }
+}
